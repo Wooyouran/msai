25956dd7d5fa89704f68dc289ec1d9ca0d69769f
Wooyouran <youran.woo@gmail.com>
2025-09-16 16:26:50 +0900

    Azure webapp 관련 sh 수정

---
+++ streamlit.sh
@@ -0,0 +1,7 @@
+pip install streamlit
+pip install python-dotenv
+pip install pandas
+pip install openai
+
+# Streamlit 실행
+streamlit run streamlit_app.py --server.port 8000 --server.address 0.0.0.0 --server.headless true
